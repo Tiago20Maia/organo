a06a936db595945403972bb9bec52ecda0469ed5
tiagolenovo <tiagoferreira209@gmail.com>
2024-04-29 15:21:13 -0300

    Criando o formulário

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import Banner from "./components/Banner";
-import TextField from "./components/TextField";
+import Form from "./components/Form";
 
 const App = () => {
   return (
     <div>
       <Banner />
-      <TextField label="Nome" placeholder="Digite o seu nome" />
-      <TextField label="Cargo" placeholder="Digite o seu cargo" />
-      <TextField label="Imagem" placeholder="Digite o seu endereço da imagem" />
+      <Form />
     </div>
   );
 };
---
+++ src/components/Form/index.js
@@ -0,0 +1,19 @@
+import TextField from "../TextField";
+
+const Form = () => {
+  return (
+    <section className="form">
+      <form>
+        <h2>Preencha os dados para criar o card do colaborador:</h2>
+        <TextField label="Nome" placeholder="Digite o seu nome" />
+        <TextField label="Cargo" placeholder="Digite o seu cargo" />
+        <TextField
+          label="Imagem"
+          placeholder="Digite o seu endereço da imagem"
+        />
+      </form>
+    </section>
+  );
+};
+
+export default Form;

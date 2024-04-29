66ef57030d3d6b0afbace66d06620f5b1063df73
tiagolenovo <tiagoferreira209@gmail.com>
2024-04-29 13:40:37 -0300

    Criando o componente de textos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import Banner from "./components/banner/banner";
+import Banner from "./components/Banner";
+import TextField from "./components/TextField";
 
 const App = () => {
   return (
     <div>
       <Banner />
+      <TextField />
     </div>
   );
 };

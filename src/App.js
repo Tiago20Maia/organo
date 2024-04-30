c5eaca054ef4900b8b8d2ee587b3e034b8f68233
tiagolenovo <tiagoferreira209@gmail.com>
2024-04-30 16:22:49 -0300

    Criando o componente Time

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Banner from "./components/Banner";
 import Form from "./components/Form";
+import Time from "./components/Time";
 
 const App = () => {
   const [employees, setEmployees] = useState([]);
@@ -14,6 +15,9 @@ const App = () => {
     <div className="App">
       <Banner />
       <Form onNewEmployeeCreate={(employees) => onNewEmployeeAdd(employees)} />
+      <Time name="Programação" />
+      <Time name="Front-End" />
+      <Time name="Data Science" />
     </div>
   );
 };
---
+++ src/components/Time/index.js
@@ -0,0 +1,11 @@
+import "./index.css";
+
+const Time = (props) => {
+  return (
+    <section className="time">
+      <h3>{props.name}</h3>
+    </section>
+  );
+};
+
+export default Time;
